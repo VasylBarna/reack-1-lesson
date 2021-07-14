c48584c5d774db26c2b6bcf27c6a56b8b685c97d
Vasyl Barna <you@example.com>
2021-07-14 21:03:40 +0300

    lesson2 style complead

--- src/App.js
+++ src/App.js
@@ -32,36 +32,36 @@ const products = [
   },
 ];
 
-// const products2 = [
-//   {
-//     id: 1,
-//     name: "Espresso",
-//     price: 2,
-//     image:
-//       "https://media.istockphoto.com/photos/hot-coffee-on-wooden-table-picture-id1084020754?s=612x612",
-//   },
-//   {
-//     id: 2,
-//     name: "Latte",
-//     price: 2.5,
-//     image:
-//       "https://www.cancer.org/content/dam/cancer-org/images/photographs/single-use/espresso-coffee-cup-with-beans-on-table-restricted.jpg",
-//   },
-//   {
-//     id: 3,
-//     name: "Capuccino",
-//     price: 1.5,
-//     image:
-//       "https://thursdaydinners.com/wp-content/uploads/2019/07/c-3-768x549.jpg",
-//   },
-//   {
-//     id: 4,
-//     name: "Kruasan",
-//     price: 1,
-//     image:
-//       "https://www.recept.ua/files/uploads/rec_img/kruassani-iz-droggevogo-testa.jpg",
-//   },
-// ];
+const products2 = [
+  {
+    id: 1,
+    name: "Espresso",
+    price: 2,
+    image:
+      "https://media.istockphoto.com/photos/hot-coffee-on-wooden-table-picture-id1084020754?s=612x612",
+  },
+  {
+    id: 2,
+    name: "Latte",
+    price: 2.5,
+    image:
+      "https://www.cancer.org/content/dam/cancer-org/images/photographs/single-use/espresso-coffee-cup-with-beans-on-table-restricted.jpg",
+  },
+  {
+    id: 3,
+    name: "Capuccino",
+    price: 1.5,
+    image:
+      "https://thursdaydinners.com/wp-content/uploads/2019/07/c-3-768x549.jpg",
+  },
+  {
+    id: 4,
+    name: "Kruasan",
+    price: 1,
+    image:
+      "https://www.recept.ua/files/uploads/rec_img/kruassani-iz-droggevogo-testa.jpg",
+  },
+];
 
 function App() {
   return (
@@ -70,14 +70,14 @@ function App() {
         <header className="App-header">
           <p>Reack {21 + 8} group!!! You are the very best! 👍</p>
           <p>This is our brand new coffee shop!</p>
-          <ProductList products={products} price={9.99}>
-            <h2 style={{ color: "tomato" }}>Елитная подборка кофе!!!</h2>
+          <ProductList priceColor="teal" products={products} price={9.99}>
+            <h2>Елитная подборка кофе!!!</h2>
             <h3>Second children</h3>
           </ProductList>
-          {/* <ProductList products={products2}>
-            <h2 style={{ color: "green" }}>Обичная подборка кофе!!!</h2>
+          <ProductList priceColor="tomato" products={products2} price={9.99}>
+            <h2>Обичная подборка кофе!!!</h2>
             <h3>Second children</h3>
-          </ProductList> */}
+          </ProductList>
         </header>
       </div>
       <h2>Second element</h2>
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,18 +1,48 @@
 import PropTypes from "prop-types";
+import styles from "./ProductList.module.scss";
+// import { Button } from "./styled";
+import Card from "@material-ui/core/Card";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "./Button";
 
-export const ProductList = ({ children, price, products }) => {
+const useStyles = makeStyles({
+  root: {
+    minWidth: "300px",
+    padding: "2rem",
+    marginBottom: "1rem",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export const ProductList = ({ children, priceColor, price, products }) => {
+  const classes = useStyles();
   return (
     <div>
       {children}
       <h2>{price}$</h2>
-      <ul>
+      <ul className={styles.ProductList}>
+        {/* <ul className={styles.ProductList}> */}
         {products.map((product) => (
           <li key={product.id}>
-            <h3>{product.name}</h3>
-            <img width="120" src={product.image} alt={product.name} />
-            <div>
-              Price: <b>{product.price}$</b>
-            </div>
+            <Card className={classes.root}>
+              {/* <li key={product.id} className={styles.ProductList__Item}> */}
+              <h3>{product.name}</h3>
+              <img width="100" src={product.image} alt={product.name} />
+              <div>
+                Price: <b>{product.price}$</b>
+              </div>
+              <Button
+                like="💅💅💅"
+                action={() => alert("Clicked")}
+                primary
+                letterSpacing="9px"
+              >
+              Buy
+              </Button>
+            </Card>
           </li>
         ))}
       </ul>
@@ -29,6 +59,7 @@ ProductList.propTypes = {
       image: PropTypes.string,
     })
   ),
+  priceColor: PropTypes.string,
 };
 
 ProductList.defaultProps = {
